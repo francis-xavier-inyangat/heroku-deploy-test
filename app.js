e63a9f2d54b2d5e8ac18719826a6c3f36163e9ac
francis-xavier-inyangat <xavierfranzings@gmail.com>
2022-08-26 23:07:35 +0300

    live on heroku!

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Heroku Live!');
+  res.send('Live on Heroku!');
 });
 app.listen(process.env.PORT || 3000, function () {
   console.log('listening on port 3000!');
